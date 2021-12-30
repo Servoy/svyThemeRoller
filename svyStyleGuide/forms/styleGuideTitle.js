b8670432f50715efcbce4dd05e08cb55b0dddd2c
Paolo <paronne@servoy.com>
2021-12-30 14:07:31 +0200

    Title for the Theme Roller

---
+++ svyStyleGuide/forms/styleGuideTitle.js
@@ -0,0 +1,25 @@
+
+/**
+ * @param {JSEvent} event
+ * @param {string} dataTarget
+ *
+ * @protected
+ *
+ * @properties={typeid:24,uuid:"AB38929C-DC2C-48EE-9A61-ACEED923A7B3"}
+ */
+function onActionThemeWiki(event, dataTarget) {
+	application.showURL("https://github.com/Servoy/svyThemeRoller/wiki", "_blank");
+
+}
+
+/**
+ * @param {JSEvent} event
+ * @param {string} dataTarget
+ *
+ * @protected
+ *
+ * @properties={typeid:24,uuid:"64FFE5B6-1683-473E-A024-DE771644AACF"}
+ */
+function onActionLessWiki(event, dataTarget) {
+	application.showURL("https://wiki.servoy.com/pages/viewpage.action?pageId=31752222", "_blank");
+}
