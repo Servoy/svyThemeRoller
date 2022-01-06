dc2e25bc4b88257c92123052fc4af5675b422468
lnabarogu <lnabarogu@servoy.com>
2022-01-06 12:55:44 +0200

    SVYX-386 Fixed merge

--- svyStyleGuide/forms/GroupListComponent.js
+++ svyStyleGuide/forms/GroupListComponent.js
@@ -1,7 +1,7 @@
 /**
  * @param {JSEvent} event
  * @param {string} dataTarget
- *
+ * @private
  * @properties={typeid:24,uuid:"7137FB4D-102D-4468-BA95-9DCB7F6919F6"}
  */
 function onActionResetCardDefault(event, dataTarget) {
