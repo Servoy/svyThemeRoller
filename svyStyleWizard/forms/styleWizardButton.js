34c1102518c8309711e686b02a015dbae6fb891c
Paolo <paronne@servoy.com>
2022-03-24 18:33:17 +0100

    Underline/Bold

--- svyStyleWizard/forms/styleWizardButton.js
+++ svyStyleWizard/forms/styleWizardButton.js
@@ -62,6 +62,20 @@ var marginLeft = '-1';
 var expandMarginSelector = 0;
 
 /**
+ * @type {Boolean}
+ *
+ * @properties={typeid:35,uuid:"85B95592-3C57-4DDD-A749-C7B5ECE51823",variableType:-4}
+ */
+var styleBold = false;
+
+/**
+ * @type {Boolean}
+ * 
+ * @properties={typeid:35,uuid:"67024D79-6C40-42F0-9269-959615120987",variableType:-4}
+ */
+var styleUnderline = false;
+
+/**
  * Callback method for when form is shown.
  *
  * @param {Boolean} firstShow form is shown first time after load
@@ -110,6 +124,23 @@ function updateUI() {
 	elements.fcMarginLeft.visible = showAllMaginSelectors;
 	elements.labelMarginAll.visible = !showAllMaginSelectors;
 	elements.labelMarginEqual.visible = showAllMaginSelectors;
+
+	if (styleBold) {
+		elements.labelBold.removeStyleClass("btn-outline-default")
+		elements.labelBold.addStyleClass("btn-primary")
+	} else {
+		elements.labelBold.addStyleClass("btn-outline-default")
+		elements.labelBold.removeStyleClass("btn-primary")
+	}
+	
+	
+	if (styleUnderline) {
+		elements.labelUnderline.removeStyleClass("btn-outline-default")
+		elements.labelUnderline.addStyleClass("btn-primary")
+	} else {
+		elements.labelUnderline.addStyleClass("btn-outline-default")
+		elements.labelUnderline.removeStyleClass("btn-primary")
+	}
 }
 
 /**
@@ -130,6 +161,14 @@ function getStyleClasses() {
 		classes = scopes.ngUtils.addStyleClass(classes, scopes.svyStyleWizard.getMarginStyleClass(marginRight, scopes.svyStyleWizard.SIDES.RIGHT));
 		classes = scopes.ngUtils.addStyleClass(classes, scopes.svyStyleWizard.getMarginStyleClass(marginBottom, scopes.svyStyleWizard.SIDES.BOTTOM));
 		classes = scopes.ngUtils.addStyleClass(classes, scopes.svyStyleWizard.getMarginStyleClass(marginLeft, scopes.svyStyleWizard.SIDES.LEFT));
+	}
+
+	if (styleBold) {
+		classes = scopes.ngUtils.addStyleClass(classes, "font-weight-bold");
+	}
+	
+	if (styleUnderline) {
+		classes = scopes.ngUtils.addStyleClass(classes, "text-underline");
 	}
 
 	return classes
@@ -167,6 +206,16 @@ function setStyleClasses(classes) {
 	if (cls.indexOf("btn-round") > -1) {
 		styleRoundedBorder = 1;
 	}
+	
+	// Bold
+	if (cls.indexOf("font-weight-bold") > -1) {
+		styleBold = true;
+	}
+	
+	// Undeline
+	if (cls.indexOf("text-underline") > -1) {
+		styleUnderline = true;
+	}
 
 	// button style
 	var key;
@@ -187,7 +236,7 @@ function setStyleClasses(classes) {
 			break;
 		}
 	}
-	
+
 	// margin top
 	for (i = 0; i < marginTopStyles.length; i++) {
 		key = marginTopStyles[i];
@@ -207,7 +256,7 @@ function setStyleClasses(classes) {
 			break;
 		}
 	}
-	
+
 	// margin bottom
 	for (i = 0; i < marginBottomStyles.length; i++) {
 		key = marginBottomStyles[i];
@@ -217,7 +266,7 @@ function setStyleClasses(classes) {
 			break;
 		}
 	}
-	
+
 	// margin left
 	for (i = 0; i < marginLeftStyles.length; i++) {
 		key = marginLeftStyles[i];
@@ -289,4 +338,33 @@ function onActionSaveAsNew() {
 	if (saveAsNew()) {
 		globals.showForm(forms.styleButtons);
 	}
+}
+
+/**
+ * @param {JSEvent} event
+ * @param {string} dataTarget
+ *
+ * @protected
+ *
+ * @properties={typeid:24,uuid:"B8ABE340-8E49-458F-A18D-DBB98F276AAE"}
+ */
+function onActionToggleBold(event, dataTarget) {
+	styleBold = !styleBold;
+	updateUI();
+	updateElementStyle(getStyleClasses());
+}
+
+
+/**
+ * @param {JSEvent} event
+ * @param {string} dataTarget
+ *
+ * @protected
+ *
+ * @properties={typeid:24,uuid:"D1D50750-5B2E-43E8-AFCC-81625B38EC92"}
+ */
+function onActionToggleUnderline(event, dataTarget) {
+	styleUnderline = !styleUnderline;
+	updateUI();
+	updateElementStyle(getStyleClasses());
 }
