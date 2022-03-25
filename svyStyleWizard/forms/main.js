e8e24fd4346858d4341270e4544cf16a3b0a40e4
Paolo <paronne@servoy.com>
2022-03-25 13:56:48 +0100

    Hide Navbar

---
+++ svyStyleWizard/forms/main.js
@@ -0,0 +1,16 @@
+/**
+ * @properties={typeid:24,uuid:"3D278F43-F22A-4475-8965-5C0DB464F05C"}
+ * @override
+ */
+function loadNavbarItems() {
+	return [];
+}
+
+
+/**
+ * @properties={typeid:24,uuid:"0A68B3A4-B116-421F-806E-60281793EF4B"}
+ * @override
+ */
+function loadMenuItems() {
+	return [];
+}
